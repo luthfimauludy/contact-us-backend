81833306c080f3865714651ed05663991ad7d39f
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2025-03-21 20:34:32 +0700

    import routes

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const cors = require("cors");
 const express = require("express");
 const dotenv = require("dotenv");
 
+// Import Routes
+const Auth = require("./server/api/auth");
+
 dotenv.config();
 
 const app = express();
@@ -11,6 +14,9 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Route Middlewares
+app.use("/api", Auth);
 
 app.get("/api", (req, res) => {
   res.send("Hello!");
